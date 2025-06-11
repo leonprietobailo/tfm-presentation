406b3d7e286ad8296ec2c71571e98a532c4ae43d
Prieto Bailo, León E <leonprietobailo@gmail.com>
2025-06-11 20:56:42 +0200

    add home screen. change background scope to app component. fix header styles. revert background square color.

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,19 @@
 import { $t } from '@primeng/themes';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   imports: [],
   templateUrl: './home.component.html',
-  styleUrl: '../slide-main-styles.scss'
+  styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+
+  constructor(private router: Router) { }
+
+ goIntro() {
+    this.router.navigate(['introduction']);
+  }
 
 }
