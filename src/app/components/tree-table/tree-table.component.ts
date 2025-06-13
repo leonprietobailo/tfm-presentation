6eb103d8096bb837fde0098e5956c3fdc7f75a6b
Prieto Bailo, León E <leonprietobailo@gmail.com>
2025-06-14 00:49:57 +0200

    add tree table component

--- src/app/components/tree-table/tree-table.component.ts
+++ src/app/components/tree-table/tree-table.component.ts
@@ -1,238 +1,45 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { TreeNode } from 'primeng/api';
 import { TreeTableModule } from 'primeng/treetable';
 
 @Component({
   selector: 'app-tree-table',
-  imports: [TreeTableModule],
+  imports: [TreeTableModule, HttpClientModule],
   templateUrl: './tree-table.component.html',
   styleUrl: './tree-table.component.scss'
 })
 export class TreeTableComponent {
   files: TreeNode[] = [];
-  cols: any[] = [];
+  cols = [
+    { field: 'name', header: 'Name', width: '40%' },
+    { field: 'count', header: 'Count', width: '20%' },
+    { field: 'size', header: 'Size', width: '20%' },
+    { field: 'type', header: 'Type', width: '20%' }
+  ];
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
-    this.cols = [
-      { field: 'name', header: 'Name' },
-      { field: 'size', header: 'Size' },
-      { field: 'type', header: 'Type' }
-    ];
-
-    this.files = [
-      {
-        data: {
-          name: "Documents",
-          size: "75kb",
-          type: "Folder"
-        },
-        children: [
-          {
-            data: {
-              name: "Work",
-              size: "55kb",
-              type: "Folder"
-            },
-            children: [
-              {
-                data: {
-                  name: "Expenses.doc",
-                  size: "30kb",
-                  type: "Document"
-                }
-              },
-              {
-                data: {
-                  name: "Resume.doc",
-                  size: "25kb",
-                  type: "Document"
-                }
-              }
-            ]
-          },
-          {
-            data: {
-              name: "Home",
-              size: "20kb",
-              type: "Folder"
-            },
-            children: [
-              {
-                data: {
-                  name: "Invoices.txt",
-                  size: "20kb",
-                  type: "Text"
-                }
-              }
-            ]
-          }
-        ]
-      },
-      {
-        data: {
-          name: "Pictures",
-          size: "150kb",
-          type: "Folder"
-        },
-        children: [
-          {
-            data: {
-              name: "Vacation",
-              size: "145kb",
-              type: "Folder"
-            },
-            children: [
-              {
-                data: {
-                  name: "Image1.png",
-                  size: "45kb",
-                  type: "Image"
-                }
-              },
-              {
-                data: {
-                  name: "Image2.png",
-                  size: "100kb",
-                  type: "Image"
-                }
-              },
-              {
-                data: {
-                  name: "Image2.png",
-                  size: "100kb",
-                  type: "Image"
-                }
-              },
-              {
-                data: {
-                  name: "Image2.png",
-                  size: "100kb",
-                  type: "Image"
-                }
-              },
-              {
-                data: {
-                  name: "Image2.png",
-                  size: "100kb",
-                  type: "Image"
-                }
-              },
-              {
-                data: {
-                  name: "Image2.png",
-                  size: "100kb",
-                  type: "Image"
-                }
-              },
-              {
-                data: {
-                  name: "Image2.png",
-                  size: "100kb",
-                  type: "Image"
-                }
-              },
-              {
-                data: {
-                  name: "Image2.png",
-                  size: "100kb",
-                  type: "Image"
-                }
-              },
-              {
-                data: {
-                  name: "Image2.png",
-                  size: "100kb",
-                  type: "Image"
-                }
-              },
-              {
-                data: {
-                  name: "Image2.png",
-                  size: "100kb",
-                  type: "Image"
-                }
-              },
-              {
-                data: {
-                  name: "Image2.png",
-                  size: "100kb",
-                  type: "Image"
-                }
-              },
-              {
-                data: {
-                  name: "Image2.png",
-                  size: "100kb",
-                  type: "Image"
-                }
-              },
-              {
-                data: {
-                  name: "Image2.png",
-                  size: "100kb",
-                  type: "Image"
-                }
-              },
-              {
-                data: {
-                  name: "Image2.png",
-                  size: "100kb",
-                  type: "Image"
-                }
-              },
-              {
-                data: {
-                  name: "Image2.png",
-                  size: "100kb",
-                  type: "Image"
-                }
-              },
-              {
-                data: {
-                  name: "Image2.png",
-                  size: "100kb",
-                  type: "Image"
-                }
-              },
-              {
-                data: {
-                  name: "Image2.png",
-                  size: "100kb",
-                  type: "Image"
-                }
-              },
-              {
-                data: {
-                  name: "Image2.png",
-                  size: "100kb",
-                  type: "Image"
-                }
-              },
-              {
-                data: {
-                  name: "Image2.png",
-                  size: "100kb",
-                  type: "Image"
-                }
-              },
-              {
-                data: {
-                  name: "Image2.png",
-                  size: "100kb",
-                  type: "Image"
-                }
-              },
-              {
-                data: {
-                  name: "Image2.png",
-                  size: "100kb",
-                  type: "Image"
-                }
-              }
-            ]
-          }
-        ]
-      }
-    ];
+    this.http.get<any[]>('assets/data/file-tree.json').subscribe(data => {
+      this.files = this.mapToTreeNodes(data);
+    });
   }
+
+  mapToTreeNodes(data: any[]): TreeNode[] {
+    return data.map(item => ({
+      data: {
+        name: item.name,
+        expand: true,
+        count: item.count,
+        size: item.size
+          ? item.size >= 1024 * 1024
+            ? `${(item.size / (1024 * 1024)).toFixed(1)} MB`
+            : `${Math.round(item.size / 1024)} KB`
+          : '', type: item.type
+      },
+      children: item.children ? this.mapToTreeNodes(item.children) : undefined
+    }));
+  }
+
 }
