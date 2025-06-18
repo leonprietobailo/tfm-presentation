e28827abf7a5ea1aceb81f51e81510c8c8fe3479
León E. Prieto Bailo <leonprietobailo@gmail.com>
2025-06-18 20:40:31 +0200

    add gradcam

--- src/app/pages/gradcam/gradcam.component.ts
+++ src/app/pages/gradcam/gradcam.component.ts
@@ -1,11 +1,51 @@
 import { Component } from '@angular/core';
+import { SlideshowSectionComponent } from "../../components/slideshow-section/slideshow-section.component";
+import { ImageCompareModule } from 'primeng/imagecompare';
+import { SelectButtonModule } from 'primeng/selectbutton';
+import { FormsModule } from '@angular/forms';
+
+
 
 @Component({
   selector: 'app-gradcam',
-  imports: [],
+  imports: [SlideshowSectionComponent, ImageCompareModule, SelectButtonModule, FormsModule],
   templateUrl: './gradcam.component.html',
-  styleUrl: '../slide-main-styles.scss'
+  styleUrls: ['../slide-main-styles.scss', './gradcam.component.scss'],
 })
 export class GradcamComponent {
+  
+  stateOptions: any[] = [{ label: 'Original', value: 'original' },{ label: 'GradCAM', value: 'gradcam' }];
+
+  originalImages: string[] = [
+    'assets/images/gradcam/comparison/cast_def_0_7.jpeg',
+    'assets/images/gradcam/comparison/cast_def_0_149.jpeg',
+    'assets/images/gradcam/comparison/cast_def_0_157.jpeg',
+    'assets/images/gradcam/comparison/cast_def_0_290.jpeg'
+  ];
+  
+  gradcamImages: string[] = [
+    'assets/images/gradcam/comparison/evident_casting_data_test_cast_def_0_7.jpeg',
+    'assets/images/gradcam/comparison/evident_casting_data_test_cast_def_0_149.jpeg',
+    'assets/images/gradcam/comparison/evident_casting_data_test_cast_def_0_157.jpeg',
+    'assets/images/gradcam/comparison/evident_casting_data_test_cast_def_0_290.jpeg'
+  ];
+  
+  displayedImages: string[] = [...this.originalImages];
+  imagesVisible = true;
+  
+  set value(val: 'original' | 'gradcam') {
+    this._value = val;
+    this.switchImages(val);
+  }
+  get value() {
+    return this._value;
+  }
+  private _value: 'original' | 'gradcam' = 'original';
+  
+  switchImages(type: 'original' | 'gradcam') {
+    this.displayedImages =
+      type === 'original' ? this.originalImages : this.gradcamImages;
+  }
 
 }
+
